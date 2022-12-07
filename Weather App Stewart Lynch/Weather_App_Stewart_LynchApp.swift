39ee789db1ac165fb12db510056a6a1ff2729bde
Baris Karalar <b.karalar@jemix.de>
2022-12-07 14:36:20 +0100

    Initial Commit

---
+++ Weather App Stewart Lynch/Weather_App_Stewart_LynchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_App_Stewart_LynchApp.swift
+//  Weather App Stewart Lynch
+//
+//  Created by Baris Karalar on 07.12.22.
+//
+
+import SwiftUI
+
+@main
+struct Weather_App_Stewart_LynchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
